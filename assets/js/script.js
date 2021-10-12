8ce33e0dfb2b199d9241e41d9df50b17d9b4940e
mzxz <mohamed.abaker110@gmail.com>
2021-10-12 05:14:50 +0000

    add code to get randoum images when the game start.

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,9 +18,22 @@ let images = ['assets/images/approximately.png',
 let gameArea = document.getElementById('game-area');
 let allDivs = document.getElementsByTagName('div');
 
-function shuffle(){
+/**
+ * Get randoum images when the game start.
+ */
+ function shuffle(array){
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while(currentIndex !== 0){
+        randomIndex = Math.floor(Math.random()*currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
+    return array;
 }
+shuffle(images);
 
 /**
  * Dislay all the images
