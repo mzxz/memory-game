d8c3fbcf6abbad6200979a15d77df46a83b13ec4
mzxz <mohamed.abaker110@gmail.com>
2021-10-12 04:41:08 +0000

    Add some global variables and images array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log('hello');
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+// Global Variables
+let images = ['assets/images/approximately.png',
+              'assets/images/divide.png',
+              'assets/images/equal.png',
+              'assets/images/percent.png',
+              'assets/images/plus.png',
+              'assets/images/times.png',
+              'assets/images/approximately.png',
+              'assets/images/divide.png',
+              'assets/images/equal.png',
+              'assets/images/percent.png',
+              'assets/images/plus.png',
+              'assets/images/times.png'];
+
+let gameArea = document.getElementById('game-area');
+let allDivs = document.getElementsByTagName('div');
